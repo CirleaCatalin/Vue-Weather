26063c16a6b2beff2788d7296577256de2367251
Catalin Cirlea <catalin.cirlea@ppb.com>
2022-06-21 21:27:06 +0300

    Created the components

---
+++ src/components/Modal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 class="modal">Modal</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+.modal{
+  border: 1px solid firebrick;
+  height: 200px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+ <header class="header">
+   <h1 id="title">Weather App</h1>
+   <control-panel></control-panel>
+ </header>
+</template>
+
+<script>
+import Control from './Control.vue'
+  export default {
+  components:{
+    'control-panel': Control
+  },
+  data () {
+    return {
+    }
+  }
+
+}
+</script>
+
+<style>
+header{
+  height: 100px;
+  border: 1px solid darkolivegreen;
+  display: flex;
+  margin-bottom: 30px;
+  font-size: 1.5em;
+  align-items: center;
+  justify-content: space-between;
+  /* box-shadow: 0 2px 4px 0 rgba(0,0,0,.1) */
+}
+
+
+</style>
